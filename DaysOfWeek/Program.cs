3c09c0715810d9b31deec41a1c44fb413804ce22
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-06-01 14:14:09 -0400

    w3resource exercise #6 complete

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,40 @@
+﻿// Write a program in C# Sharp to display the name of the days of the week.
+// Expected Output:
+// Sunday
+// Monday
+// Tuesday
+// Wednesday
+// Thursday
+// Friday
+// Saturday
+
+using System;
+using System.Linq;
+
+namespace Days
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] days =
+            {
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday"
+            };
+
+            var day = from d in days 
+            select d;
+
+            foreach (var d in day)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
