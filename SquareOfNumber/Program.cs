c9f649260e4a5db7946b730f1f2767d4947f7feb
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-06-01 13:49:40 -0400

    w3resource exercise #3 complete

---
+++ SquareOfNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿// Write a program in C# Sharp to find the number of an array and the square of each number.
+// Expected Output :
+// { Number = 9, SqrNo = 81 }
+// { Number = 8, SqrNo = 64 }
+// { Number = 6, SqrNo = 36 }
+// { Number = 5, SqrNo = 25 }
+
+using System;
+using System.Linq;
+
+namespace SquareOfNumber {
+    class Program {
+        static void Main ()
+        {
+            var number = new int[] { 9, 8, 6, 5 };
+
+            var numberQuery = from num in number
+                              let sqrNum = num * num
+                              select new { Number = num, SqrNo = sqrNum };
+
+            foreach (var num in numberQuery) {
+                Console.WriteLine (num);
+            }
+        }
+    }
+}
